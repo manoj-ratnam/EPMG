5f83c25c76637217f8c3cc2b7eaa08104e4e431d
Manoj Jayarathinam <jayarathinam.manoj@marks-and-spencer.com>
2020-02-01 12:29:08 +0000

    Added 3 tier environment creation terraform code

---
+++ 3-tier-aws-application/variables.tf
@@ -0,0 +1,45 @@
+# remote configuration
+terraform {
+  required_version = ">= 0.11.1"
+
+  backend "s3" {
+    encrypt = "true"
+  }
+}
+
+# aws provider
+provider "aws" {
+  region = "us-east-2"
+  access_key = "AKIAWVQFQQ2WSG5XKLVU"
+  secret_key = "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"
+}
+
+# fetching coreos AMI
+data "aws_ami" "coreos" {
+  filter {
+    name   = "name"
+    values = ["CoreOS-stable-1576.4.0-hvm"] # Fixed value to avoid recreation of instances
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["595879546273"] # CoreOS
+}
+
+# path to ssh public key
+variable "public_key_path" {
+  default = "~/.ssh/3-tier.pub"
+}
+
+# name of the key to be used
+variable "key_name" {
+  default = "manoj-ssh-key"
+}
+
+# EC2 machine instance type 
+variable "instance_type" {
+  default = "t2.micro"
+}
